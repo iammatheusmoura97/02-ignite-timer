4dd2efafbc67fee57c765f54ff114144f336e92b
Matheus Moura <matheusmoura07@hotmail.com>
2024-03-18 15:34:38 -0300

    Estrutura da aplicação - rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Button } from './components/Button.tsx'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router.tsx'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant={'primary'} />
-      <Button variant={'secondary'} />
-      <Button variant={'success'} />
-      <Button variant={'danger'} />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div>
+      <h1>HEADER</h1>
+    </div>
+  )
+}
